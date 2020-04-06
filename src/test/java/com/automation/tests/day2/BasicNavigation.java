38646aa2d9b9eaefc8238b505f2df85f679208f3
EmmaDefne <serenity3347@gmail.com>
2020-04-06 00:16:32 -0500

    day 2 added more

--- src/test/java/com/automation/tests/day2/BasicNavigation.java
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -1,12 +1,17 @@
 package com.automation.tests.day2;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BasicNavigation {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriverManager.chromedriver().setup();
-        ChromeDriver driver=new ChromeDriver();
+        WebDriver driver= new ChromeDriver();
+
+        driver.get("http://google.com");
+        Thread.sleep(3000);
+        driver.close();
 
 
     }
