fe22c2a95fdb2c13bf695acac9798b5c1e33dbee
EmmaDefne <serenity3347@gmail.com>
2020-04-07 15:38:05 -0500

    more

--- src/test/java/com/automation/tests/day2/BasicNavigation.java
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -10,9 +10,38 @@ public class BasicNavigation {
         WebDriver driver= new ChromeDriver();
 
         driver.get("http://google.com");
+        driver.manage().window().maximize();
         Thread.sleep(3000);
+        String title=driver.getTitle();
+        String expected = "Google";
+        System.out.println("The title is "+title);
+        if(expected.equals(title)   ){
+            System.out.println("TEST PASSED...");
+        }else{
+            System.out.println("TEST FAILED");
+        }
+
+        driver.navigate().to("http://amazon.com");
+        if(driver.getTitle().toLowerCase().contains("amazon")){
+            System.out.println("TEST PASSED...");
+        }else{
+            System.out.println("TEST FAILED");
+
+        }
+        driver.navigate().back();
+        verifyEqual(driver.getTitle(),"Google");
+        driver.navigate().forward();
+
+        System.out.println("The title is...."+driver.getTitle());
+        System.out.println("URL====>"+driver.getCurrentUrl());
+        driver.navigate().refresh();
         driver.close();
-
-
+    }
+    public static void verifyEqual(String arg1,String arg2){
+        if(arg1.equals(arg2)){
+            System.out.println("TEST PASSED");
+        }else{
+            System.out.println("TEST FAILED");
+        }
     }
 }
