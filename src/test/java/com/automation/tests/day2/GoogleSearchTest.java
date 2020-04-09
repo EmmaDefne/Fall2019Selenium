852877226e65f37b060f9b13167db90ecdaff11d
EmmaDefne <serenity3347@gmail.com>
2020-04-09 12:55:44 -0500

    more

--- src/test/java/com/automation/tests/day2/GoogleSearchTest.java
+++ src/test/java/com/automation/tests/day2/GoogleSearchTest.java
@@ -18,19 +18,7 @@ public class GoogleSearchTest {
 
         driver.get("http://google.com");
         Thread.sleep(2000);
-
-        //By.name("q") --> name="q"
-        //name - it's one of the selenium locators
-        //there are 8 locators
-        //we use locators to find elements
-        //to choose locator, just use By.locator
         WebElement search = driver.findElement(By.name("q"));
-        //once we found an element, how to enter text?
-        //to enter text, use sendKeys() method
-        //how to press Enter after entering the text?
-        //use Key.ENTER
-        //Keys.ENTER - perform keyboard click
-        //keysToSend - name of the parameter, we don't specify it
         search.sendKeys("Java", Keys.ENTER);
         // 2000 - 2 seconds
         // 12000 - 12 seconds
